fe443c52b3b92f833af8d318107d4e631b4baf96
Kyle Sherbert <ksherb1@students.towson.edu>
2017-11-13 21:29:44 -0500

    Refactor to clean-er architecture

---
+++ entities.ts
@@ -0,0 +1,18 @@
+import { User } from './user';
+import { LearningObject } from './learning-object';
+import { Outcome, OutcomeSuggestion, LearningOutcome, StandardOutcome } from './outcome';
+import { LearningGoal } from './learning-goal';
+import { InstructionalStrategy } from './instructional-strategy';
+import { AssessmentPlan } from './assessment-plan';
+
+export {
+    User,
+    LearningObject,
+    Outcome,
+    OutcomeSuggestion,
+    LearningOutcome,
+    StandardOutcome,
+    LearningGoal,
+    InstructionalStrategy,
+    AssessmentPlan
+};
