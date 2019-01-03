97237a0a7183f64bb25d2cf9c415b76f7dd9be5d
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-03 08:57:26 -0500

    :sparkles: Add SubmittableLearningObject class

---
+++ lib/learning-object/submittable-learning-object.ts
@@ -0,0 +1,7 @@
+import { LearningObject } from './learning-object';
+
+export class SubmittableLearningObject extends LearningObject {
+  constructor(object: LearningObject) {
+    super(object);
+  }
+}
