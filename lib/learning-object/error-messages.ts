d4508e4bd7e136238e39b246c57e7c7abcc9431b
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-03 07:32:21 -0500

    :sparkles: Add more const error messages

--- lib/learning-object/error-messages.ts
+++ lib/learning-object/error-messages.ts
@@ -1,9 +1,16 @@
 export const LEARNING_OBJECT_ERRORS = {
-  INVALID_NAME: 'Name must be defined',
+  INVALID_NAME: 'Name must be defined.',
   INVALID_LENGTH(length: string) {
     if (!length) {
-      return 'Length must be defined';
+      return 'Length must be defined.';
     }
-    return `${length} is not a valid length`;
-  }
+    return `${length} is not a valid length.`;
+  },
+  LEVEL_EXISTS(level: string) {
+    return `${level} has already been added.`;
+  },
+  INVALID_LEVEL(level: string) {
+    return `${level} is not a valid level.`;
+  },
+  INVALID_LEVELS: 'Levels must contain at least one valid academic level.'
 };
