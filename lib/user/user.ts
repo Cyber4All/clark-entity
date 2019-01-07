6140a4f8332e89a86ee90930358e21c8d57665ba
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-07 06:57:34 -0500

    :recycle: Refactor User class to remove instantitate function

--- lib/user/user.ts
+++ lib/user/user.ts
@@ -1,46 +1,125 @@
 /**
- * Provide an abstract representation for a CLARK user.
- */
-
-/**
  * A class to represent CLARK users.
  * @class
  */
 export class User {
-  // Index Signature to allow extra properties;
-  [key: string]: any;
+  _username: string;
   /**
-   * @property {string} id a user's unique log-in username
+   * @property {string} username a user's unique log-in username
    */
-  username: string;
+  get username(): string {
+    return this._username;
+  }
 
+  _name: string;
   /**
    * @property {string} name a user's real-life name
    */
-  name: string;
+  get name(): string {
+    return this._name;
+  }
+  set name(name: string) {
+    if (name && name.trim()) {
+      this._name = name.trim();
+    }
+  }
 
+  _email: string;
   /**
    * @property {string} email a user's email on file
    */
-  email: string;
+  get email(): string {
+    return this._email;
+  }
+  set email(email: string) {
+    if (email) {
+      this._email = email;
+    }
+  }
 
+  _emailVerified: boolean;
+  /**
+   * @property {boolean} emailVerified a user's email on file
+   */
+  get emailVerified(): boolean {
+    return this._emailVerified;
+  }
+  set emailVerified(emailVerified: boolean) {
+    if (emailVerified) {
+      this._emailVerified = emailVerified;
+    }
+  }
+
+  _organization: string;
   /**
    * @property {string} organization a user's associate organization
    */
-  organization: string;
+  get organization(): string {
+    return this._organization;
+  }
+  set organization(organization: string) {
+    if (organization && organization.trim()) {
+      this._organization = organization;
+    }
+  }
 
+  _bio: string;
   /**
-   * Returns User's Bio
-   *
-   * @type {string}
-   * @memberof User
+   * @property {string} bio a user's bio
    */
-  bio: string;
-
+  get bio(): string {
+    return this._bio;
+  }
+  set bio(bio: string) {
+    if (bio) {
+      this._bio = bio.trim();
+    }
+  }
   /**
    * @property {string} createdAt timestamp of user entity creation
    */
-  createdAt: string;
+  _createdAt: string;
+  get createdAt(): string {
+    return this._createdAt;
+  }
+
+  /**
+   *Creates an instance of User.
+   * @param {Partial<User>} [user]
+   * @memberof User
+   */
+  constructor(user?: Partial<User>) {
+    this._username = '';
+    this._name = '';
+    this._email = '';
+    this._emailVerified = false;
+    this._organization = '';
+    this._bio = '';
+    this._createdAt = Date.now().toString();
+    if (user) {
+      this.copyUser(user);
+    }
+  }
+
+  /**
+   * Copies properties of user to this user if defined
+   *
+   * @private
+   * @param {Partial<User>} user
+   * @memberof User
+   */
+  private copyUser(user: Partial<User>): void {
+    this._username = user.username || this.username;
+    this.name = user.name || this.name;
+    if (user.email) {
+      this.email = <string>user.email;
+    }
+    this._emailVerified = user.emailVerified || this.emailVerified;
+    this.organization = user.organization || this.organization;
+    this.bio = user.bio || this.bio;
+    this._createdAt = user.createdAt || this.createdAt;
+  }
+}
 
   /**
    * Construct a new User, given starting user id and name.
