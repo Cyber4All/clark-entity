b2ff1abbddbb8660500502ff73a0758dbce0069c
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-07 06:22:39 -0500

    :fire: Remove InstructionalStrategy and AssessmentPlan

--- lib/index.ts
+++ lib/index.ts
@@ -4,8 +4,6 @@ import { Outcome, OutcomeSuggestion } from './outcome/outcome';
 import { StandardOutcome } from './standard-outcome/standard-outcome';
 import { LearningOutcome } from './learning-outcome/learning-outcome';
 import { LearningGoal } from './learning-goal';
-import { InstructionalStrategy } from './instructional-strategy';
-import { AssessmentPlan } from './assessment-plan';
 import { Collection } from './collection/collection';
 import { Rating } from './rating/rating';
 
@@ -18,8 +16,6 @@ export {
   LearningOutcome,
   StandardOutcome,
   LearningGoal,
-  InstructionalStrategy,
-  AssessmentPlan,
   Collection,
   Rating
 };
